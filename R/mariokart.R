5e2ccf40074cf05c09b54f571726cc18abe818e2
David J. Hunter <dhunter@westmont.edu>
2022-12-03 13:36:05 -0800

    mario kart carpentry

--- R/mariokart.R
+++ R/mariokart.R
@@ -32,5 +32,47 @@ ggplot(mk, aes(x = total_pr, y = cond)) + geom_violin()
 
 ## Control for Wii wheels
 
-## Challenge: Control for wheels, stock photo, and duration
+glimpse(mk)
+
+ggplot(mk, aes(x = wheels, y = total_pr)) + geom_point()
+ggplot(mk, aes(x = wheels, y = total_pr, color = cond)) + geom_point()
+
+## Challenge: Control for wheels by adding it as a predictor. Call your new
+## model mmod2. Compare with mmod1.
+
+## Solution:
+mmod2 <- lm(total_pr ~ cond + wheels, data = mk)
+summary(mmod2)
+
+# The average price difference is only $5, when wheels are taken into account.
+
+## Cross Validation
+
+
+set.seed(3456)
+tc <- trainControl(method = "cv", number = 5)
+train(total_pr ~ cond, data = mk, method = "lm", trControl = tc)
+train(total_pr ~ cond + wheels, data = mk, method = "lm", trControl = tc)
+
+## Challenge: Find a model for predicting total_pr with a lower RMSE.
+
+## Solution
+train(total_pr ~ cond + wheels + start_pr, data = mk, method = "lm", trControl = tc)
+train(total_pr ~ cond + wheels + seller_rate, data = mk, method = "lm", trControl = tc)
+train(total_pr ~ cond + wheels + start_pr + seller_rate, data = mk, method = "lm", trControl = tc)
+train(total_pr ~ cond + wheels + start_pr + stock_photo, data = mk, method = "lm", trControl = tc)
+
+## Challenge: For the best model you found, 
+## 1. Are all the predictors significant? 
+## 2. Record the adjusted R^2.
+
+## Solution
+mmod3 <- lm(total_pr ~ cond + wheels + start_pr + seller_rate, data = mk)
+summary(mmod3)
+
+## Challenge: make some plots illustrating the important predictors that you found.
+
+## Solution:
+ggplot(mk, aes(x = start_pr, y = total_pr)) + geom_point() + geom_smooth()
+ggplot(mk, aes(y = stock_photo, x = total_pr)) + geom_violin() + geom_boxplot()
 
