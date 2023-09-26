7a74a3efc9a1d523112e141bae5f0bc92bba232d
David J. Hunter <dhunter@westmont.edu>
2023-09-26 14:53:48 -0700

    Updated logistic regression exercise

--- R/logit_regression_nba.R
+++ R/logit_regression_nba.R
@@ -8,14 +8,11 @@ ggplot(nba, aes(x = PTS)) + geom_histogram()
 ggplot(nba, aes(x = PTS, color = TARGET_5Yrs)) + geom_histogram()
 ggplot(nba, aes(x = PTS, fill = TARGET_5Yrs)) + geom_histogram()
 ggplot(nba, aes(x = PTS, fill = TARGET_5Yrs)) + 
-  geom_histogram(position = "dodge", binwidth = 3)
+  geom_histogram(position = "dodge", binwidth = 100)
 ggplot(nba, aes(x = PTS, y = TARGET_5Yrs)) + geom_point()
-ggplot(nba, aes(x = PTS, y = TARGET_5Yrs)) + geom_boxplot() # doesn't work
 
-## Convert response variable to a factor
-nba$TARGET_5Yrs <- factor(nba$TARGET_5Yrs)
-
-ggplot(nba, aes(x = PTS, y = TARGET_5Yrs)) + geom_boxplot()
+## Convert response variable to a factor: Not needed for logi
+# nba$TARGET_5Yrs <- factor(nba$TARGET_5Yrs)
 
 ## Logistic regression models
 bbmod1 <- glm(TARGET_5Yrs ~ PTS, family = "binomial", data = nba)
@@ -27,41 +24,48 @@ summary(bbmod1)
 #GP GS MIN  FGM-FGA FG%  3PTM-3PA 3P%  FTm-FTA FT%  OR  DR  REB AST BLK STL PF  TO  PTS
 #77 40 27.4 3.6-6.9 52.0 0.9-2.4  37.6 0.7-1.0 67.9 0.4 2.4 2.8 3.3 0.4 0.8 2.4 1.4 8.8
 
+## Ayo Dosunmu scored 678 points in his rookie year.
 ## Predict if Ayo will be in the league in 5 years:
-predict(bbmod1, data.frame(PTS = 8.8))
-exp(1.064908)/(1+exp(1.06498))
-predict(bbmod1, data.frame(PTS = 8.8), type = "response")
+predict(bbmod1, data.frame(PTS = 678))
+exp(2.242346)/(1+exp(2.242346))
+predict(bbmod1, data.frame(PTS = 678), type = "response")
+# 0.90
 
-## Challenge: Add games played  (G) as another predictor in your model. (bbmod2)
-## Ayo played 77 games as a rookie and averaged 8.8 points. What
-## probability does the model predict?
+## Challenge: Add total rebounds (TRB) as another predictor in your model. (bbmod2)
+## Ayo scored 678 points as a rookie and had 216 rebounds. What
+## probability does the model predict? Compare to bbmod1. Does it make sense?
 
 # Solution
-bbmod2 <- glm(TARGET_5Yrs ~ PTS + G, family = "binomial", data = nba)
+bbmod2 <- glm(TARGET_5Yrs ~ PTS + TRB, family = "binomial", data = nba)
 summary(bbmod2)
-predict(bbmod2, data.frame(PTS = 8.8, G = 77), type = "response")
-# 0.823
+predict(bbmod2, data.frame(PTS = 678, TRB = 216), type = "response")
+# 0.86
 
-## Challenge: make a scatterplot of G vs PTS, colored by TARGET_5Yrs
+## Challenge: make a scatterplot of TRB vs PTS, colored by TARGET_5Yrs
 ## What do you notice? Where would Ayo be?
 ## Solution
-ggplot(nba, aes(x = PTS, y = G, color = TARGET_5Yrs)) +
+ggplot(nba, aes(x = PTS, y = TRB, color = TARGET_5Yrs)) +
   geom_point() +
-  geom_smooth() +
   scale_color_brewer(palette = "Set2")
+
+## Challenge: Who is the highest-scoring rookie in this data set? (Use filter)
+nba %>% filter(PTS > 2000) %>% glimpse()
 
 ## Challenge: Try making a model with more than 5 predictor 
 # variables (your choice). Use the step function on your model.
 # Does it eliminate any variables?
 # Solution:
-model3 <- glm(TARGET_5Yrs ~ PTS + G + AST + TRB + TOV, 
+model3 <- glm(TARGET_5Yrs ~ PTS + G + AST + TRB + TOV + ORB, 
               family = "binomial", data = nba)
 step(model3)
 
 ## Challenge: how could you display three variables in a plot,
 # giving some information about 5yr success?
-ggplot(nba, aes(x = PTS, y = G, size = TRB, color = TARGET_5Yrs)) +
+ggplot(nba, aes(x = PTS, y = TRB, size = AST, color = TARGET_5Yrs)) +
   geom_point(alpha = 0.5) +
   scale_color_brewer(palette = "Set1")
+
+## Challenge: When did they move the 3-point line in? Make a scatterplot
+## to help you decide.
 
 
