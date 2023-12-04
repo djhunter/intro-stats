a91b0e1dd30b0921f039d58d818c502782c5ffdd
David J. Hunter <dhunter@westmont.edu>
2023-12-04 08:33:24 -0800

    update nba rookies

--- R/logitinfer.R
+++ R/logitinfer.R
@@ -7,43 +7,47 @@ view(nba)
 
 ggplot(nba, aes(x = PTS)) + geom_histogram()
 ggplot(nba, aes(x = PTS, y = TARGET_5Yrs)) + geom_point()
-ggplot(nba, aes(x = PTS, y = TARGET_5Yrs)) + geom_boxplot() # doesn't work
-
-## Convert response variable to a factor
-nba$TARGET_5Yrs <- factor(nba$TARGET_5Yrs)
-
 ggplot(nba, aes(x = PTS, y = TARGET_5Yrs)) + geom_boxplot()
 
 ## Logistic regression models
 bbmod1 <- glm(TARGET_5Yrs ~ PTS, family = "binomial", data = nba)
 summary(bbmod1)
-bbmod2 <- glm(TARGET_5Yrs ~ 3P Made, family = "binomial", data = nba) # error
+
+bbmod2 <- glm(TARGET_5Yrs ~ 3P%, family = "binomial", data = nba) # error
 
 glimpse(nba)
-bbmod2 <- glm(TARGET_5Yrs ~ `3P Made`, family = "binomial", data = nba)
+bbmod2 <- glm(TARGET_5Yrs ~ `3P%`, family = "binomial", data = nba)
 summary(bbmod2)
 
 # Challenge: Improve bbmod2 by controlling for 3 point attempts (3PA). Do
 # the p-values change? Can you explain why?
 
 ## Solution:
-bbmod3 <- glm(TARGET_5Yrs ~ `3P Made` + `3PA`, family = "binomial", data = nba)
+bbmod3 <- glm(TARGET_5Yrs ~ `3P%` + `3PA`, family = "binomial", data = nba)
 summary(bbmod3)
 
 ## Correlated predictors?
-cor(nba[,2:19])
-round(cor(nba[,2:19]), 2)
+cor(nba[,7:31])
+round(cor(nba[,7:31]), 1)
 
-ggplot(nba, aes(x = `3PA`, y = `3P Made`)) + geom_point()
-ggplot(nba, aes(x = `3P%`)) + geom_histogram()
+## Challenge: Why are some of the correlations NA? 
 
 ## Cross Validation
 
 tc <- trainControl(method = "cv", number = 10)
-bbcv1 <- train(TARGET_5Yrs ~ PTS, 
+train(TARGET_5Yrs ~ PTS,  data = nba, 
+      method = "glm", family = "binomial", trControl = tc)
+nba <- nba %>%
+  mutate(T5Y = factor(ifelse(TARGET_5Yrs, "Yes", "No")))
+train(T5Y ~ PTS,  data = nba, 
+      method = "glm", family = "binomial", trControl = tc)
+bbcv1 <- train(T5Y ~ PTS, 
                data = nba, method = "glm", family = "binomial", trControl = tc)
 confusionMatrix(bbcv1)
-bbcv2 <- train(TARGET_5Yrs ~ `3P Made` + `3PA`, 
+
+bbcv2 <- train(T5Y ~ `3P%` + `3PA`, 
+               data = nba, method = "glm", family = "binomial", trControl = tc)
+bbcv2 <- train(T5Y ~ `3P` + `3PA`, 
                data = nba, method = "glm", family = "binomial", trControl = tc)
 confusionMatrix(bbcv2)
 bbcv2
@@ -53,33 +57,29 @@ bbcv2
 ## table. Which predictors are most important? 
 
 ## Solution?
-bbcv3 <- train(TARGET_5Yrs ~ GP + PTS + OREB + AST, 
+tc <- trainControl(method = "repeatedcv", number = 10, repeats = 10)
+bbcv3 <- train(T5Y ~ G + PTS + ORB + AST, 
                data = nba, method = "glm", family = "binomial", trControl = tc)
 confusionMatrix(bbcv3)
 
-bbmod3 <- glm(TARGET_5Yrs ~ GP + PTS + OREB + AST, family = "binomial", data = nba)
+bbmod3 <- glm(TARGET_5Yrs ~ G + PTS + ORB + AST, family = "binomial", data = nba)
 summary(bbmod3)
 
 ## Search for best model?
 nba_complete <- nba %>%
-  drop_na() %>%
-  select(-Name)
-fullmod <- glm(TARGET_5Yrs ~ ., family = "binomial", data = nba_complete)
+  select(-TARGET_5Yrs, -PlayerID, -Player, -Pos, -Age, -Tm, -Year, -`FG%`, -`3P%`, -`2P%`, -`eFG%`, -`FT%`, -GS)
+fullmod <- glm(T5Y ~ ., family = "binomial", data = nba_complete)
 summary(fullmod)
 step(fullmod)
-bestmod <- glm(TARGET_5Yrs ~ GP + MIN + FGA + `FG%` + `3P Made` + 
-                 `3PA` + FTM + `FT%` + OREB + AST + BLK + TOV, 
+
+## Challenge: Use the results of step, make a regression model using glm
+## and test its accuracy using train. Does it beat the other models we found?
+
+bestmod <- glm(T5Y ~ G + MP + PTS + FGA + DRB + AST + BLK + TOV + PF, 
                family = "binomial", data = nba_complete)
 summary(bestmod)
 summary(fullmod)
 
-nba_nona <- nba %>%
-  select(-Name, -`3P%`)
-fullmod <- glm(TARGET_5Yrs ~ ., family = "binomial", data = nba_nona)
-summary(fullmod)
-step(fullmod)
-
-bbcv4 <- train(TARGET_5Yrs ~ GP + `FG%` + `3P Made` + `3PA` + 
-                 `FT%` + DREB + REB + AST + BLK, 
+bbcv4 <- train(T5Y ~ G + MP + PTS + FGA + DRB + AST + BLK + TOV + PF, 
                data = nba, method = "glm", family = "binomial", trControl = tc)
 confusionMatrix(bbcv4)
