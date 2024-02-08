c8b2e85937cbded6649c5dfa968469d3bd5c6640
David J. Hunter <dhunter@westmont.edu>
2024-02-08 09:19:43 -0800

    update bootstrapping carpentry to use real data

--- R/bootstrapping.R
+++ R/bootstrapping.R
@@ -1,25 +1,21 @@
 library(tidyverse)
 
-# Sample data: Poll of 50 people. 
-#    Do you attend church regularly? (Yes, No)
-#    20% respond yes
-polldata <- data.frame(
-  church = c(rep("Yes", 10), rep("No", 40))
-)
+gss <- read_csv("http://math.westmont.edu/data/gss.csv")
 
-glimpse(polldata)
-polldata %>%
+glimpse(gss)
+
+gss %>% 
   count(church)
 
-ggplot(polldata, aes(x = church)) +
+ggplot(gss, aes(x = church)) +
   geom_bar()
 
-polldata %>%
-  summarize(phat = sum(church == "Yes")/n())
+gss %>%
+  summarize(phat = sum(church == "often")/300)
 
 ## Create a random resample
-samp1 <- polldata %>%
-  slice_sample(n = 50, replace = TRUE)
+samp1 <- gss %>%
+  slice_sample(n = 300, replace = TRUE)
 
 samp1 %>%
   count(church)
@@ -30,32 +26,38 @@ samp1 %>%
 # 2. Summarize to compute p_hat for this sample
 
 ######### Solution
-polldata %>%
-  slice_sample(n = 50, replace = TRUE) %>%
+gss %>%
+  slice_sample(n = 300, replace = TRUE) %>%
   count(church)
 
-polldata %>%
-  slice_sample(n = 50, replace = TRUE) %>%
-  summarize(p_hat = sum(church == "Yes")/n())
+gss %>%
+  slice_sample(n = 300, replace = TRUE) %>%
+  summarize(p_hat = sum(church == "often")/300)
 #######
 
 ## Make lots of p_hats
 
 makephat <- function(i) {
-  polldata %>%
-  slice_sample(n = 50, replace = TRUE) %>%
-  summarize(p_hat = sum(church == "Yes")/n())
+  gss %>%
+  slice_sample(n = 300, replace = TRUE) %>%
+  summarize(p_hat = sum(church == "often")/300)
 }
 
 ## Try it out:
 makephat()
 makephat()
 
+## Use map to repeat the function:
+map(1:5, makephat)
+
+map(1:5, makephat) %>%
+  bind_rows()
+
 phatDF <- map(1:100, makephat) %>%
   bind_rows()
 
 ggplot(phatDF, aes(x = p_hat)) +
-  geom_histogram(binwidth = 0.05)
+  geom_histogram()
 
 ## Challenge: 
 ## 1. Modify the above code to create a sampling distribution with 1000 p_hats.
@@ -69,25 +71,32 @@ ggplot(phatDF, aes(x = p_hat)) +
   geom_density()
 #######
 
-## Summarize the distribution
+## Create a confidence interval
 
 phatDF %>%
   summarize(p025 = quantile(p_hat, 0.025),
             p975 = quantile(p_hat, 0.975))
 
-### Challenge: Redo the bootstrapping simulation, but this time
-#              use a sample size of 500 instead of 50, with the same proportion
-#              of churchgoers (20%). How does the 95% CI change?
+## Interpretation: If we repeated the survey many times, 95% of the 
+## phats would fall between 0.397 and 0.51.
 
-############## Solution:
-polldata <- data.frame(
-  church = c(rep("Yes", 100), rep("No", 400))
+gss %>%
+  count(church)
+
+## Simulated data: sample size of 3000 instead of 300.
+biggss <- data.frame(
+  church = c(rep("often", 1360), rep("rarely", 1640))
 )
 
+### Challenge: Redo the bootstrapping simulation, but this time
+#              use a biggss simulated data. How does the 95% CI change?
+
+############## Solution:
+
 makephat <- function(i) {
-  polldata %>%
-  slice_sample(n = 500, replace = TRUE) %>%
-  summarize(p_hat = sum(church == "Yes")/n())
+  biggss %>%
+  slice_sample(n = 3000, replace = TRUE) %>%
+  summarize(p_hat = sum(church == "often")/3000)
 }
 
 phatDF <- map(1:1000, makephat) %>%
@@ -95,9 +104,13 @@ phatDF <- map(1:1000, makephat) %>%
 
 ggplot(phatDF, aes(x = p_hat)) +
   geom_density()
+#######
+
+## Create a confidence interval
 
 phatDF %>%
   summarize(p025 = quantile(p_hat, 0.025),
             p975 = quantile(p_hat, 0.975))
+
 
 ###############################################
